cdbfc26042a693f3baf3e1ce995db4b359ea4bff
naveen kumar <naveenkumar@naveens-MacBook-Air.local>
2024-03-03 21:59:39 +0530

    index js update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: `${process.env.NODE_APP_URL}`,
+    origin: `http://localhost:3000`,
   })
 );
 app.use(cookieParser());
